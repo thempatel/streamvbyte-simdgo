448bc9967cd12053d30d69dacea61944f8ee760a
theMPatel <bicelot3@gmail.com>
2021-08-31 14:02:58 -0400

    generation check

---
+++ tools/generate_and_check.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BEFORE_DIFF=$(git diff | sha1sum )
+BEFORE_STATUS=$(git status --porcelain | sha1sum)
+
+make generate
+
+AFTER_DIFF=$(git diff | sha1sum )
+AFTER_STATUS=$(git status --porcelain | sha1sum)
+
+if [[ $BEFORE_DIFF != $AFTER_DIFF || $BEFORE_STATUS != $AFTER_STATUS ]]; then
+  echo "Unstable generate. Make sure to generate and check in changed files."
+  exit 1
+fi
