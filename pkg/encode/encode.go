93daea91c65eeb16f7132c51bf0f396e60490e30
theMPatel <bicelot3@gmail.com>
2021-09-01 10:27:27 -0400

    init

--- pkg/encode/encode.go
+++ pkg/encode/encode.go
@@ -24,10 +24,20 @@ func init() {
 	}
 }
 
+// Put8uint32 is a general func you can use to encode 8 uint32's at a time.
+// It will use the fastest implementation available determined during
+// package initialization. If your CPU supports special hardware instructions
+// then it will use an accelerated version of Stream VByte. Otherwise, the
+// scalar implementation will be used as the fallback.
 func Put8uint32(in []uint32, out []byte) uint16 {
 	return putImpl(in, out)
 }
 
+// Put8uint32Scalar will encode 8 uint32 values from in into out using the
+// Stream VByte format. Returns an 16-bit control value produced from the
+// encoding.
+// Note: It is your responsibility to ensure that the incoming slices have
+// the appropriate sizes and data otherwise this func will panic.
 func Put8uint32Scalar(in []uint32, out []byte) uint16 {
 	var ctrl uint16
 	first := Put4uint32Scalar(in, out)
@@ -37,6 +47,33 @@ func Put8uint32Scalar(in []uint32, out []byte) uint16 {
 	return ctrl | uint16(second)<<8
 }
 
+// Put4uint32Scalar will encode 4 uint32 values from in into out using the
+// Stream VByte format. Returns an 8-bit control value produced from the
+// encoding. Every incoming number is variably encoded, and an 8-bit control
+// is constructed from the 2-bit len of each uint32. Below is an example of
+// 4 uint32's and how they are encoded.
+//
+// 00000000 00000000 00000000 01101111  =        111
+// 00000000 00000000 00000100 11010010  =       1234
+// 00000000 00001100 00001010 10000011  =     789123
+// 01000000 00000000 00000000 00000000  = 1073741824
+//
+// Num         Len      2-bit control
+// ----------------------------------
+// 111          1                0b00
+// 1234         2                0b01
+// 789123       3                0b10
+// 1073741824   4                0b11
+//
+// Final Control byte
+// 0b11100100
+//
+// Encoded data (little endian right-to-left bottom-to-top)
+// 0b01000000 0b00000000 0b00000000 0b00000000 0b00001100
+// 0b00001010 0b10000011 0b00000100 0b11010010 0b01101111
+//
+// Note: It is your responsibility to ensure that the incoming slices have
+// the appropriate sizes and data otherwise this func will panic.
 func Put4uint32Scalar(in []uint32, out []byte) uint8 {
 	// bounds check hint to compiler
 	_ = in[3]
@@ -54,6 +91,15 @@ func Put4uint32Scalar(in []uint32, out []byte) uint8 {
 	return uint8((len0 - 1) | (len1-1)<<2 | (len2-1)<<4 | (len3-1)<<6)
 }
 
+// Put8uint32DiffScalar will differentially encode 8 uint32 values from in into out.
+// Prev provides a way for you to indicate the base value for this batch of 8.
+// For example, when encoding the second batch of 8, you would provide a prev
+// value of 80, to ensure that the integers are correctly resolved to the correct
+// diff.
+//
+// Original:     [ 10, 20, 30, 40, 50, 60, 70, 80 ] [ 90, 100, 110, 120, 130, 140, 150, 160 ]
+// Differential: [ 10, 10, 10, 10, 10, 10, 10, 10 ] [ 10, 10, 10, 10, 10, 10, 10, 10 ]
+// Prev: 80
 func Put8uint32DiffScalar(in []uint32, out []byte, prev uint32) uint16 {
 	var ctrl uint16
 	first := Put4uint32DiffScalar(in, out, prev)
